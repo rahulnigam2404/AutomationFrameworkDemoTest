ea0953a2e2794a0bf9f906aeb7b41f0bcacec9af
rahulnigam2404 <rahulnigam2404@gmail.com>
2024-12-17 12:40:51 +0530

    Update FirstProgtest.java

--- Home/src/test/java/FirstProgtest.java
+++ Home/src/test/java/FirstProgtest.java
@@ -3,7 +3,7 @@ import org.testng.annotations.Test;
 public class FirstProgtest {
 	
 	//public static void main (String args[]) {
-	
+	//changes for pull
 	@Test
 	public void firstProgtest() {	
 	System.out.println("First Program runned successfully");
