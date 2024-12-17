89da6099d03cd5ba9994bb6f9e0f4b0460e0a4b2
rahulnigam2404 <rahulnigam2404@gmail.com>
2024-12-17 12:42:51 +0530

    push changes

--- Home/src/test/java/FirstProgtest.java
+++ Home/src/test/java/FirstProgtest.java
@@ -4,6 +4,7 @@ public class FirstProgtest {
 	
 	//public static void main (String args[]) {
 	//changes for pull
+	//changes for push
 	@Test
 	public void firstProgtest() {	
 	System.out.println("First Program runned successfully");
